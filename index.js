d4f8650ffb344a91698ef8f35cffdb8c7c88531d
John Garbero <john.garbero.itb@gmail.com>
2022-04-18 08:19:26 -0500

    commit tree 6f69a793a67f188fe279f6726ca77443f4d0a211

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+
+function whisper(string) {
+    return string.toLowerCase();
+}
+
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string) {
+    if (console.log(string === string.toLowerCase())) {
+        result = `I can't hear you!`
+    } else if (console.log(string ===string.toUpperCase())) {
+        result = `YES INDEED!`
+    } else if (console.log(string === "Let's have dinner together!")) {
+        result = `I would love to!`
+    } return result
+}
